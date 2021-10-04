e9f12850728a92c0c71b7c41f0baf319ac084c9f
kemiksizt <87303268+kemiksizt@users.noreply.github.com>
2021-10-04 23:09:36 +0300

    GuessNumber.java
    Basic guess number

---
+++ java-101/AverageofArray.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int[] numbers = {10, 20, 30, 40, 50};
+        int sum = 0;
+        
+        for(int i = 0; i < numbers.length; i++){
+            sum += numbers[i];
+        }
+        
+        
+        System.out.println(sum/numbers.length);
+    }
+    
+}
